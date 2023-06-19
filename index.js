284aa1e516e4568d2e3463bd020e4d9b8594cdfa
Dhruva Panyam <dhruvapanyam16@gmail.com>
2023-06-19 12:07:00 +0530

    create database connection

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 
+// Connect to the `contacts.db` database if exists. If not, create the db file...
+const db = new sqlite3.Database('./databases/contacts.db', (err) => {
+    if(err){
+        throw err;
+    }
+    
+    console.log(`Connected to the CONTACTS database!`)
+})
