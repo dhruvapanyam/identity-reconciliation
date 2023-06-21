f9bf94b6b97bb2bcf75c5a56070c2ba4851d5a5e
Dhruva Panyam <dhruvapanyam16@gmail.com>
2023-06-21 16:32:11 +0530

    process port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const PORT = 8080 || process.env.PORT
+
 app.get('/', (req, res) => {
     return res.json([1,2,3])
 })
@@ -27,7 +29,7 @@ app.post('/clear', (req, res) => {
     return res.json();
 })
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080...')
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
 })
 
